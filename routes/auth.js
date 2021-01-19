49bede89b03fe91ddb6a1e4fe1169966eed237fc
Prashant <prashant.niroula67@gmail.com>
2021-01-19 23:30:17 +0545

    jwt auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,29 @@
+require('dotenv').config();
+
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const auth = require('../middleware/auth');
 
 router.get('/', (req, res) => res.json({ msg: 'Login Page' }));
+
+router.get('/login', auth, (req, res) => {
+  User.findById(req.user.id)
+    .select('-password')
+    .then((user) => {
+      if (user) {
+        res.json(user);
+      } else {
+        res.send('user does not exist');
+      }
+    })
+    .catch((err) => {
+      // console.log(err);
+      res.status(500).send({ msg: 'Server Error' });
+    });
+});
 
 router.post('/login', (req, res) => {
   User.findOne({ email: req.body.email })
@@ -11,10 +31,19 @@ router.post('/login', (req, res) => {
       if (!user) {
         return res.status(400).send('Incorrect email or password');
       }
-      if (req.body.password != user.password) {
-        return res.status(400).send('Incorrect password');
+      const isMatch = bcrypt.compare(req.body.password, user.password);
+      if (!isMatch) {
+        return res.status(400).send({ msg: 'Incorrect password' });
       }
-      res.send(true);
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      });
       // const validPassword = bcrypt.compare(req.body.password, user.password);
       // if (!validPassword) {
       //   return res.status(400).send('Incorrect password');
--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,38 @@
+require('dotenv').config();
+
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.get('/', (req, res) => res.json({ msg: 'create a user' }));
 
 router.post('/register', (req, res) => {
+  const { name, email, password } = req.body;
   User.findOne({ email: req.body.email }).then((user) => {
     if (user) {
       return res.status(400).send('User already exists');
     } else {
       const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
+        name,
+        email,
+        password,
       });
       bcrypt.hash(user.password, 10).then((hash) => {
         user.password = hash;
         user
           .save()
           .then((user) => {
-            res.json({ msg: 'User registered' });
+            const payload = {
+              user: {
+                id: user.id,
+              },
+            };
+            jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, (err, token) => {
+              if (err) throw err;
+              res.json({ token });
+            });
           })
           .catch((err) => console.log(err));
       });
